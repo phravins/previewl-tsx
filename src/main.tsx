56127041e3153c93f97279b6a5ccc716ed02932d
phravins <phravintreuno@gmail.com>
2026-04-10 16:20:35 +0530

    feat: add src/main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
+import './index.css';
+
+console.log('Main component loading - FULL VERSION');
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
+  </StrictMode>,
+);
